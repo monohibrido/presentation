9d064a8a16c518c439ff2913b955f3b9a30438e8
Cristian Orellana Vega <orellana.vega.cristian@gmail.com>
2025-03-31 23:59:22 -0300

    update routes 2

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,13 +3,13 @@ import { Link } from "react-router-dom";
 function NavBar() {
   return (
     <nav className="nav">
-      <Link className="nav-link" to="/presentation/">
+      <Link className="nav-link" to="/">
         CURRÍCULUM VITAE
       </Link>
-      <Link className="nav-link" to="/presentation/skills">
+      <Link className="nav-link" to="/skills">
         Skills and Projects
       </Link>
-      <Link className="nav-link" to="/presentation/experience">
+      <Link className="nav-link" to="/experience">
         Experience
       </Link>
     </nav>
